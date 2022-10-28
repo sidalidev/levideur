56810b8a0b8629a405f0d257d2fdce3119daf22e
sidbentifraouine@gmail.com <Sid Ali Bentifraouine>
2022-10-29 01:41:56 +0200

    signup: jwt

--- main.ts
+++ main.ts
@@ -3,6 +3,8 @@ import morgan from "morgan"
 import cors from "cors"
 import { PrismaClient } from "@prisma/client"
 import bcrypt from "bcrypt"
+import jwt from "jsonwebtoken"
+const SECRET = "secret"
 
 const PORT = 1234
 
@@ -33,13 +35,14 @@ app.post("/signup", async (req, res) => {
   // Je gen un mdp crypté
   const crypted_password = await bcrypt.hash(req.body.password, salt)
 
-  const user = await prisma.user.create({
+  const { id, email } = await prisma.user.create({
     data: {
       email: req.body.email,
       password: crypted_password,
     },
   })
-  res.send(user)
+  const access_token = jwt.sign({ id }, SECRET, { expiresIn: "3 hours" })
+  res.send({ id, email, token: access_token })
 })
 
 app.listen(PORT, () => {
