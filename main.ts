9df2fcfae2ab058c283eeed5c573317d2353469f
sidbentifraouine@gmail.com <Sid Ali Bentifraouine>
2022-10-28 22:40:38 +0200

    first route

--- main.ts
+++ main.ts
@@ -1,23 +1,32 @@
+import express from "express"
+import morgan from "morgan"
+import cors from "cors"
 import { PrismaClient } from "@prisma/client"
 
+const PORT = 1234
+
+// Initialisation de Express 4
+const app = express()
+
+// Activation de CORS pour les CORS...
+app.use(cors())
+// Activation de Morgan pour les logs
+app.use(morgan("tiny"))
+// Activation du raw (json)
+app.use(express.json())
+// Activation de x-wwww-form-urlencoded
+
+app.use(express.urlencoded({ extended: true }))
+
+// Initialisation du client Prisma
 const prisma = new PrismaClient()
 
-async function main() {
-  const user = await prisma.user.create({
-    data: {
-      name: "Alice",
-      email: "alice@prisma.io",
-    },
-  })
-  console.log(user)
-}
+// Requete GET /
+app.get("/", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.send(users)
+})
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`)
+})
